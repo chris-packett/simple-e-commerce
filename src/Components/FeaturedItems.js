d46cf5665c8bf053cc9b864b6c705a014e152edf
Chris <chrispackett1998@gmail.com>
2018-08-26 19:45:58 -0400

    refactored FeaturedItems component and added random featured items func

--- src/Components/FeaturedItems.js
+++ src/Components/FeaturedItems.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 import data from '../data.json'
 
+import { Link } from 'react-router-dom'
+
 class FeaturedItems extends Component {
     render() {
         return (
             <section className="featured-items">
-                {/* {
+                {
                     Object.keys(data).map((objKey, i) => {
-                        for (let j = 0; j < 2; j++) {
-                            return (
-                                <section className="featured-item" key={i+j}>
-                                    <img src={data[objKey].collection[i].imageURL} alt={data[objKey].collection[i].name} />
-                                    <div>{data[objKey].collection[i].name}</div>
-                                    <div className="italic">{data[objKey].collection[i].price}</div>
-                                </section>
-                            )
-                        }
+                        let new_id = Math.floor(Math.random() * 4)
+                        return (
+                            <section className="featured-item" key={i}>
+                                <Link to={`/${objKey}/${new_id}`}><img src={data[objKey].collection[new_id].imageURL} alt={data[objKey].collection[new_id].name} /></Link>
+                                <div>{data[objKey].collection[new_id].name}</div>
+                                <div className="italic">{data[objKey].collection[new_id].price}</div>
+                            </section>
+                        )
                     })
-                } */}
-                <section className="featured-item">
-                    <img src={data['watches'].collection[0].imageURL} alt={data['watches'].collection[0].name} />
-                    <div>{data['watches'].collection[0].name}</div>
-                    <div className="italic">{data['watches'].collection[0].price}</div>
-                </section>                
-                <section className="featured-item">
-                    <img src={data['glasses'].collection[0].imageURL} alt={data['glasses'].collection[0].name} />
-                    <div>{data['glasses'].collection[0].name}</div>
-                    <div className="italic">{data['glasses'].collection[0].price}</div>
-                </section>
-                <section className="featured-item">
-                    <img src={data['glasses'].collection[3].imageURL} alt={data['glasses'].collection[3].name} />
-                    <section className="featured-item-info">
-                        <div>{data['glasses'].collection[3].name}</div>
-                        <div className="italic">{data['glasses'].collection[3].price}</div>
-                    </section>
-                </section>
-                <section className="featured-item">
-                    <img src={data['watches'].collection[1].imageURL} alt={data['watches'].collection[1].name} />
-                    <div>{data['watches'].collection[1].name}</div>
-                    <div className="italic">{data['watches'].collection[1].price}</div>
-                </section>
+                }
+                {
+                    Object.keys(data).reverse().map((objKey, i) => {
+                        let new_id = Math.floor(Math.random() * 4)
+                        console.log(new_id)
+                        return (
+                            <section className="featured-item" key={i}>
+                                <Link to={`/${objKey}/${new_id}`}><img src={data[objKey].collection[new_id].imageURL} alt={data[objKey].collection[new_id].name} /></Link>
+                                <div>{data[objKey].collection[new_id].name}</div>
+                                <div className="italic">{data[objKey].collection[new_id].price}</div>
+                            </section>
+                        )
+                    })   
+                }
             </section>
         );
     }
